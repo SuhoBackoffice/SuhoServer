93c468575b809507e4ba0ca50c9759f3f6bbae13
KangBaekGwa <ksu9801@gmail.com>
2025-04-21 11:07:41 +0900

    project setting

---
+++ src/main/java/baekgwa/suhoserver/SuhoServerApplication.java
@@ -0,0 +1,13 @@
+package baekgwa.suhoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuhoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuhoServerApplication.class, args);
+	}
+
+}
